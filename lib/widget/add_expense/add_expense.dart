8e0dc63211ccc486cf817332f14ab9aa347fc72e
dattq2 <Shieldo392@>
2024-10-16 16:57:12 +0700

    add bugs

--- lib/widget/add_expense/add_expense.dart
+++ lib/widget/add_expense/add_expense.dart
@@ -8,16 +8,34 @@ class AddExpense extends StatefulWidget {
 }
 
 class _AddExpenseState extends State<AddExpense> {
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _titleController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
-            decoration: InputDecoration(
-                label: Text("Label"), hintTextDirection: TextDirection.ltr),
+            decoration: const InputDecoration(label: Text("Label")),
+          ),
+          Row(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                  },
+                  child: const Text("Save Text"))
+            ],
           )
         ],
       ),
